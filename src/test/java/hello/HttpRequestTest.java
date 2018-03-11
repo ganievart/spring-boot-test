20116286ac8a4542bcbff5f8f66468df21fe2ce2
arthur <ganievart@gmail.com>
2018-03-11 22:11:05 +0100

    overview spring boot test

---
+++ src/test/java/hello/HttpRequestTest.java
@@ -0,0 +1,33 @@
+package hello;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Test
+    public void greetingShouldReturnDefaultMessage() throws Exception {
+
+        assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/",
+                String.class)).contains("Hello world");
+    }
+
+
+
+}
